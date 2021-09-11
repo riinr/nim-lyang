0390b1a6c5cee151374a7fbe1da975f9e96a499f
hugosenari <hugosenari@gmail.com>
2021-09-11 19:54:04 -0300

    feat: configure cachix

--- nix/gh-actions.nix
+++ nix/gh-actions.nix
@@ -9,11 +9,11 @@
       - uses: cachix/install-nix-action@v13
         with:
           nix_path: nixpkgs=channel:nixos-unstable
+          extra_nix_config: |
+            experimental-features = nix-command flakes
       - uses: cachix/cachix-action@v10
         with:
           name: riinr
           authToken: "''${{ secrets.CACHIX_AUTH_TOKEN }}"
-          extra_nix_config: |
-            experimental-features = nix-command flakes
       - run: nix flake check
 ''
