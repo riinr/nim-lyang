295c10ef842f8f7af16409a5248056a954271e9b
hugosenari <hugosenari@gmail.com>
2021-09-11 22:33:50 -0300

    feat: create an flake to libyang

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.pre-commit-flake.url = "github:cachix/pre-commit-hooks.nix";
-  inputs.libyang.url = "path:./nix/libyang";
+  inputs.libyang-flake.url = "path:./nix/libyang";
 
-  outputs = { self, nixpkgs, flake-utils, pre-commit-flake, libyang }:
+  outputs = { self, nixpkgs, flake-utils, pre-commit-flake, libyang-flake }:
     let systems = [ "x86_64-linux" ];
     in
     flake-utils.lib.eachSystem systems (system:
@@ -15,7 +15,11 @@
         checks = import ./nix/checks.nix { inherit pkgs system pre-commit-hooks; };
         shellHook = checks.pre-commit-check.shellHook;
         devShell = import ./nix/shell.nix { inherit pkgs checks; };
+        libyang = libyang-flake.outputs.defaultPackage.${system};
+        packages = { libyang = libyang; };
       in
-      { inherit checks devShell; }
+      {
+        inherit checks devShell packages;
+      }
     );
 }
